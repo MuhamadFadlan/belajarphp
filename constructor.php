2bc3799b5f20aef50e26e8d1fc30a8c0a51ee724
MuhamadFadlan <63693493+MuhamadFadlan@users.noreply.github.com>
2020-08-31 09:43:15 +0700

    Add files via upload

---
+++ constructor.php
@@ -0,0 +1,29 @@
+<?php
+    class produk{
+    public $namabarang,
+             $merk,
+             $harga;
+    
+    public function getCetak(){
+            return "$this->namabarang, $this->merk ,$this->harga";
+        } 
+        public function __construct($namabarang="nama barang",$merk="merk",$harga=0){
+            $this->namabarang = $namabarang;
+            $this->merk = $merk;
+            $this->harga = $harga;
+        }               
+    }
+
+    $produk1 = new produk("ideapad s340","Lenovo",8000000);
+    $produk2 = new produk("Flashdisk","sandisk",100000);
+    $produk3 = new produk("Macbook Air","Apple",25000000);
+    $produk4 = new produk("Mouse","Logitech",1200000);
+
+    echo "Nama Barang 1: " . $produk1->getCetak();
+    echo "<br>";
+    echo "Nama barang 2: " . $produk2->getCetak();
+    echo "<br>";
+    echo "Nama Barang 3: " . $produk3->getCetak();
+    echo "<br>";
+    echo "Nama Barang 4: " . $produk4->getCetak();
+?>
